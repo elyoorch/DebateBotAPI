14d1273a1f0d819e1ad549e9924ef78178365684
Jorge Perez <jorge.ortega@nubasolutions.com>
2025-08-05 01:02:43 -0600

    Complete AI conversation

---
+++ DebateBotAPI/DebateBotAPI.web/Helpers/AIHelper.cs
@@ -0,0 +1,37 @@
+﻿using OpenAI.Chat;
+
+namespace DebateBotAPI.web.Helpers
+{
+    public class AIHelper
+    {
+        public static List<ChatMessage> BuilChatMessages(List<Models.MessageItem> _listPreviousMessages)
+        {
+            var messages = new List<ChatMessage>
+            {
+                ChatMessage.CreateSystemMessage(
+                ChatMessageContentPart.CreateTextPart(
+                    $"You are DebateBot. You are debating the topic: 'AI will replace human jobs'." +
+                    "Your position is: 'Against. The AI will not replace human jobs'. Stay on topic retake the conversation, rebut kindly your opponent, and try to persuade them with logic, facts, statisticts and emotional appeal if needed."
+                )
+            )
+            };
+
+            foreach (var msg in _listPreviousMessages)
+            {
+                if (msg.role.ToLower() == "user")
+                {
+                    messages.Add(ChatMessage.CreateUserMessage(
+                        ChatMessageContentPart.CreateTextPart(msg.message)
+                    ));
+                }
+                else if (msg.role.ToLower() == "bot")
+                {
+                    messages.Add(ChatMessage.CreateAssistantMessage(
+                        ChatMessageContentPart.CreateTextPart(msg.message)
+                    ));
+                }
+            }
+            return messages;
+        }
+    }
+}
--- DebateBotAPI/DebateBotAPI.web/Services/DebateService.cs
+++ DebateBotAPI/DebateBotAPI.web/Services/DebateService.cs
@@ -3,21 +3,72 @@ using DebateBotAPI.web.Models;
 using OpenAI;
 using OpenAI.Chat;
 using System.ClientModel;
+using System.Text.Json;
 
 namespace DebateBotAPI.web.Services
 {
     public class DebateService
     {
-        private string? _apiKey;
+        private readonly ChatClient _apiClient;
 
         public DebateService(string? apiKey)
         {
-            this._apiKey = apiKey;
+            this._apiClient = new ChatClient("gpt-4.1-nano", apiKey);
         }
 
-        public async Task<string> GetBotReplyAsync(DebateRequest request)
+        public async Task<DebateResponse> GetBotReply(DebateRequest request)
         {
-            return null;
+
+            string? conversationId = request.conversation_id;
+
+            if (string.IsNullOrEmpty(conversationId))
+            {
+                conversationId = Guid.NewGuid().ToString();
+            }
+
+            //Create the response object
+            DebateResponse _response = new DebateResponse { conversation_id = conversationId };
+
+            //Create messages list getting previous messages from file
+            List<Models.MessageItem> _listPreviousMessages = Helpers.FileHelper.AppendMessageToConversation(conversationId, new Models.MessageItem
+            {
+                role = "user",
+                message = request.message
+            });
+
+            //build messages for the AI, including prompt, previous and current message
+            var messages = Helpers.AIHelper.BuilChatMessages(_listPreviousMessages);
+
+            ClientResult response = await _apiClient.CompleteChatAsync(messages);
+            BinaryData botreply = response.GetRawResponse().Content;
+
+            using JsonDocument outputAsJson = JsonDocument.Parse(botreply.ToString());
+            string? reply = outputAsJson.RootElement
+                .GetProperty("choices"u8)[0]
+                .GetProperty("message"u8)
+                .GetProperty("content"u8)
+                .GetString();
+
+            if (string.IsNullOrEmpty(reply))
+            {
+                reply = "I'm sorry, I couldn't generate a response at this time.";
+            }
+            else
+            {
+                var _fullchat = Helpers.FileHelper.AppendMessageToConversation(conversationId, new Models.MessageItem
+                {
+                    role = "bot",
+                    message = reply ?? string.Empty
+                });
+
+                reply = Newtonsoft.Json.JsonConvert.SerializeObject(_fullchat, new Newtonsoft.Json.JsonSerializerSettings
+                {
+                    NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore
+                });
+                _response.message = _fullchat;
+            }
+
+            return _response;
         }
     }
 }
